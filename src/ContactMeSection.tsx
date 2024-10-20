6e12ab1a4a746ec9d8941fee410c816135a34e01
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-19 21:44:32 -0700

    Partially completed the contactMeSection. Currently contains completed form. Need to add images and links to my socials

---
+++ src/ContactMeSection.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function ContactMeSection() {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="mt-28 flex-1 border-2 border-orange-300"></div>
+        <h1 className="mt-28 flex justify-center px-5 font-playFair text-7xl font-bold text-white">
+          Contact Me
+        </h1>
+        <div className="mt-28 flex-1 border-2 border-orange-300"></div>
+      </div>
+    </>
+  );
+}
