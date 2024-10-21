14a633cf6de129975ab181b979d87c92d0098282
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-21 13:35:11 -0700

    Added mobile support to the about section

--- src/About.tsx
+++ src/About.tsx
@@ -1,10 +1,12 @@
 export function About() {
   return (
-    <div className="ml-28 mt-28 flex flex-col">
-      <p className="mb-10 font-playFair text-7xl font-bold text-white">About</p>
+    <div className="ml-5 mt-28 flex flex-col md:ml-28">
+      <p className="mb-10 self-center border-b-2 border-b-orange-300 pb-2 font-playFair text-7xl font-bold text-white md:self-auto md:border-none md:p-0">
+        About
+      </p>
       <div className="flex justify-center">
-        <span className="mt-4 h-0 self-start border-2 border-b-0 border-orange-300 pl-20"></span>
-        <span className="mr-52 pl-3 text-3xl text-white">
+        <span className="md:mt-4 md:h-0 md:self-start md:border-2 md:border-b-0 md:border-orange-300 md:pl-20"></span>
+        <span className="pl-3 text-2xl text-white md:mr-16 md:text-3xl lg:mr-52">
           Hello my name is Anthony Gonzalez. I’m a first-gen college student
           majoring in Computer Science at California State University Los
           Angeles. I am a front end developer with experience working with the
