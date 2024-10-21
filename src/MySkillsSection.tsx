18419674e4c77bd402c014d297cc78811c800d80
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-21 13:39:50 -0700

    Added mobile support to the skills section

--- src/MySkillsSection.tsx
+++ src/MySkillsSection.tsx
@@ -9,22 +9,21 @@ import { SkillsCard } from "./SkillsCard";
 export function MySkillsSection() {
   return (
     <>
-      <div className="mb-10 mt-28 flex justify-center font-playFair text-7xl font-bold text-white">
+      <div className="mb-10 mt-28 flex justify-center font-playFair text-5xl font-bold text-white sm:text-6xl md:text-7xl">
         My Skills
       </div>
 
-      <div className="flex justify-center space-x-10">
-        <div className="flex flex-col gap-11">
-          <SkillsCard imageComponent={<ReactSVG />}>React </SkillsCard>
-          <SkillsCard imageComponent={<HtmlSVG />}>Html </SkillsCard>
+      <div className="flex flex-wrap justify-center gap-10">
+        <div className="flex flex-col items-center gap-8 sm:flex-row sm:gap-10">
+          <SkillsCard imageComponent={<ReactSVG />}>React</SkillsCard>
+          <SkillsCard imageComponent={<HtmlSVG />}>HTML</SkillsCard>
         </div>
-        <div className="flex flex-col gap-11">
+        <div className="flex flex-col items-center gap-8 sm:flex-row sm:gap-10">
           <SkillsCard imageComponent={<TypescriptSVG />}>TypeScript</SkillsCard>
           <SkillsCard imageComponent={<CSSSVG />}>CSS</SkillsCard>
         </div>
-        <div className="flex flex-col gap-11">
+        <div className="flex flex-col items-center gap-8 sm:flex-row sm:gap-10">
           <SkillsCard imageComponent={<TailWindSVG />}>Tailwind</SkillsCard>
-
           <SkillsCard imageComponent={<JavaScriptSVG />}>JavaScript</SkillsCard>
         </div>
       </div>
