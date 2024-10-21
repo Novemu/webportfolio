b49e7b98e8c6ee3880b24d653a8f06fa3b9e0493
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-21 12:21:01 -0700

    made navbar responsive for mobile by turning navbar into a hamburger view in mobile view

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,4 +1,7 @@
 import { AntSVG } from "./SVGImages/AntSVG";
+import { Squash as Hamburger } from "hamburger-react";
+import { useState } from "react";
+2;
 
 export type NavBarProps = {
   scrollToAbout: () => void;
@@ -13,44 +16,81 @@ export function NavBar({
   scrollToContact,
   scrollToHero,
 }: NavBarProps) {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <nav className="sticky top-0 z-50 mt-5 flex items-center bg-zinc-800 pb-5 pt-5">
-      <div
-        className="absolute left-20 hover:cursor-pointer"
-        onClick={scrollToHero}
-      >
-        <AntSVG />
-      </div>
+    <>
+      <nav className="sticky top-0 z-50 mt-5 flex items-center justify-between bg-zinc-800 pb-5 pt-5">
+        <div
+          className="ml-10 hover:cursor-pointer md:ml-20"
+          onClick={scrollToHero}
+        >
+          <AntSVG />
+        </div>
 
-      <ul className="flex grow justify-center space-x-5 text-xl">
-        <li>
-          <a
-            href="#about"
-            className="cursor-pointer text-gray-400 hover:font-bold hover:text-white"
-            onClick={scrollToAbout}
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#portfolio"
-            className="cursor-pointer text-gray-400 hover:font-bold hover:text-white"
-            onClick={scrollToProjects}
-          >
-            Projects
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className="cursor-pointer text-gray-400 hover:font-bold hover:text-white"
-            onClick={scrollToContact}
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+        <ul className="hidden text-xl md:mr-20 md:flex md:grow md:justify-center md:space-x-5">
+          <li>
+            <a
+              href="#about"
+              className="cursor-pointer text-gray-400 hover:font-bold hover:text-white"
+              onClick={scrollToAbout}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#portfolio"
+              className="cursor-pointer text-gray-400 hover:font-bold hover:text-white"
+              onClick={scrollToProjects}
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className="cursor-pointer text-gray-400 hover:font-bold hover:text-white"
+              onClick={scrollToContact}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div className="flex-col pr-5 text-orange-300 md:hidden">
+          <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+        </div>
+      </nav>
+      {isOpen && (
+        <ul className="flex flex-col items-center text-xl shadow-md">
+          <li className="w-full">
+            <a
+              href="#about"
+              className="block cursor-pointer border-b border-b-orange-300 p-2 text-center text-gray-400 hover:font-bold hover:text-white"
+              onClick={scrollToAbout}
+            >
+              About
+            </a>
+          </li>
+          <li className="w-full">
+            <a
+              href="#portfolio"
+              className="block cursor-pointer border-b border-b-orange-300 p-2 text-center text-gray-400 hover:font-bold hover:text-white"
+              onClick={scrollToProjects}
+            >
+              Projects
+            </a>
+          </li>
+          <li className="w-full">
+            <a
+              href="#contact"
+              className="block cursor-pointer border-b border-b-orange-300 p-2 text-center text-gray-400 hover:font-bold hover:text-white"
+              onClick={scrollToContact}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      )}
+    </>
   );
 }
