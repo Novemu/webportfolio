7b746f99b0ef45e77e1a065a8ddbb93abc602bc0
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-20 20:59:57 -0700

    Added ref to the about section and passed the scroll to about to the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import { NavBar } from "./NavBar";
 import { MySkillsSection } from "./MySkillsSection";
 import { MyProjectsSection } from "./MyProjectsSection";
 import { ContactMeSection } from "./ContactMeSection";
+import { useRef } from "react";
 
 function App() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+
+  const scrollToAbout = () => {
+    if (aboutRef.current) {
+      aboutRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
-      <NavBar />
+      <NavBar scrollToAbout={scrollToAbout} />
       <div className="mt-14 h-screen shadow-2xl shadow-zinc-700">
         <Hero />
       </div>
+      <div ref={aboutRef}></div>
       <About />
       <MySkillsSection />
       <MyProjectsSection />
