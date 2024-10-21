182dbb521acd372a213c11dd3bdfb8171b525bc2
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-20 20:33:58 -0700

    Connected svg to my linked in profile

--- src/SVGImages/LinkedInSVG.tsx
+++ src/SVGImages/LinkedInSVG.tsx
@@ -2,18 +2,23 @@ import type { SVGProps } from "react";
 
 export function LinkedInSVG(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      {...props}
-      className="h-16 w-16 hover:cursor-pointer hover:text-orange-300"
+    <a
+      target="_blank"
+      href="https://www.linkedin.com/in/anthony-gonzalez-416426255/"
     >
-      <path
-        fill="currentColor"
-        d="M20.47 2H3.53a1.45 1.45 0 0 0-1.47 1.43v17.14A1.45 1.45 0 0 0 3.53 22h16.94a1.45 1.45 0 0 0 1.47-1.43V3.43A1.45 1.45 0 0 0 20.47 2M8.09 18.74h-3v-9h3ZM6.59 8.48a1.56 1.56 0 1 1 0-3.12a1.57 1.57 0 1 1 0 3.12m12.32 10.26h-3v-4.83c0-1.21-.43-2-1.52-2A1.65 1.65 0 0 0 12.85 13a2 2 0 0 0-.1.73v5h-3v-9h3V11a3 3 0 0 1 2.71-1.5c2 0 3.45 1.29 3.45 4.06Z"
-      ></path>
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        {...props}
+        className="mr-5 h-16 w-16 hover:cursor-pointer hover:text-orange-300"
+      >
+        <path
+          fill="currentColor"
+          d="M20.47 2H3.53a1.45 1.45 0 0 0-1.47 1.43v17.14A1.45 1.45 0 0 0 3.53 22h16.94a1.45 1.45 0 0 0 1.47-1.43V3.43A1.45 1.45 0 0 0 20.47 2M8.09 18.74h-3v-9h3ZM6.59 8.48a1.56 1.56 0 1 1 0-3.12a1.57 1.57 0 1 1 0 3.12m12.32 10.26h-3v-4.83c0-1.21-.43-2-1.52-2A1.65 1.65 0 0 0 12.85 13a2 2 0 0 0-.1.73v5h-3v-9h3V11a3 3 0 0 1 2.71-1.5c2 0 3.45 1.29 3.45 4.06Z"
+        ></path>
+      </svg>
+    </a>
   );
 }
