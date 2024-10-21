84cc6b7459f3adda0fffb81cd2b8ab977e1f755d
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-20 21:06:02 -0700

    Added reference to the project section of the page. Created a scroll function to this component and passed it to the NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { useRef } from "react";
 
 function App() {
   const aboutRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
 
   const scrollToAbout = () => {
     if (aboutRef.current) {
@@ -15,15 +16,25 @@ function App() {
     }
   };
 
+  const scrollToProjects = () => {
+    if (projectsRef.current) {
+      projectsRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
-      <NavBar scrollToAbout={scrollToAbout} />
+      <NavBar
+        scrollToAbout={scrollToAbout}
+        scrollToProjects={scrollToProjects}
+      />
       <div className="mt-14 h-screen shadow-2xl shadow-zinc-700">
         <Hero />
       </div>
       <div ref={aboutRef}></div>
       <About />
       <MySkillsSection />
+      <div ref={projectsRef}></div>
       <MyProjectsSection />
       <ContactMeSection />
     </>
