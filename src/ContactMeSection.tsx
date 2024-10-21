e986dce841741d24fab7b6f7fa0d9bf490bb71c1
Anthony Gonzalez <Gonzalez.anthony0232@gmail.com>
2024-10-20 20:38:01 -0700

    Added linked in and github links

--- src/ContactMeSection.tsx
+++ src/ContactMeSection.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { ContactForm } from "./ContactForm";
+import { GitHubSVG } from "./SVGImages/GitHubSVG";
+import { LinkedInSVG } from "./SVGImages/LinkedInSVG";
 
 export function ContactMeSection() {
   return (
@@ -7,6 +9,14 @@ export function ContactMeSection() {
         <div className="mt-28 flex-1 border-2 border-orange-300"></div>
         <h1 className="mt-28 flex justify-center px-5 font-playFair text-7xl font-bold text-white">
           Contact Me
+        </h1>
+        <div className="mt-28 flex-1 border-2 border-orange-300"></div>
+      </div>
+      <ContactForm />
+      <div className="flex items-center justify-between">
+        <div className="mt-20 flex-1 border-2 border-orange-300"></div>
+        <h1 className="mt-28 flex justify-center px-5 font-playFair text-7xl font-bold text-white">
+          <LinkedInSVG /> <GitHubSVG />
         </h1>
         <div className="mt-28 flex-1 border-2 border-orange-300"></div>
       </div>
